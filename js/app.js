a4bda237d7da31624960be8fa230c578bf1dbfd8
Akash Chavan <meakashchavan@gmail.com>
2017-01-02 15:51:39 +0530

    Display the canvas along with enemy and player on webpage
    working on the movement of the player

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,22 +28,63 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
-  this.player = 'images/char-boy.png';
+var Player = function(x,y,speed){
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/char-boy.png';
 };
-// Update the player's position, required method for game
-Player.prototype.update = function(){
+Player.prototype.update = function(dt){
 
 };
-// Draw the player on the screen, required method for game
 Player.prototype.render = function(){
-  ctx.drawImage(Resources.get(this.player),this.x,this.y);
-};
+  ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+}
+
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightpressed = false;
+Player.prototype.handleInput = function(event){
+  if(event == 37){
+    leftPressed = true;
+  }
+  if(event == 38){
+    upPressed = true;
+  }
+  if(event == 39){
+    rightpressed = true;
+  }
+  if(event == 40){
+    downPressed = true;
+  }
+
+
+  if(leftPressed){
+    this.x -= 10;
+  }
+  if(rightpressed){
+    this.x += 10;
+  }
+  if(upPressed){
+    this.y += 10;
+  }
+  if(downPressed){
+    this.y -= 10;
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var player = new Player(202.5,383,50);
+var score = 0;
+var gameLevel = 1;
+var scoreLevelDiv = document.createElement('div');
+var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
